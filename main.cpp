04774f87c6373b00d13c8e223bfc00ee3c2d1d19
Stephano <stephano.wurttele@utec.edu.pe>
2019-08-25 01:31:31 -0500

    Finished

--- main.cpp
+++ main.cpp
@@ -3,6 +3,27 @@
 using namespace std;
 
 int main() {
-	return EXIT_SUCCESS;
+	int n,n2;
+	int prev,actual=0;
+	int high,low;
+	cin>>n;
+	for(int i=0;i<n;i++){
+		high=0;
+		low=0;
+		cin>>n2;
+		cin>>actual;
+		n2--;
+		while(n2--){
+			prev=actual;
+			cin>>actual;
+			if(actual>prev)
+				high++;
+			else
+				if(prev>actual)
+					low++;
+		}
+		cout<<"Case "<<i+1<<": "<<high<<" "<<low<<endl;
+		}
+	return 0;
 }
 
